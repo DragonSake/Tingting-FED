af9974f880a8acbc1b584fddfb6da3ef6311eecb
DragonSake <40611000+DragonSake@users.noreply.github.com>
2021-10-05 18:57:32 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,15 @@
+// Met hulp van de oefening States oefening 2 208c van  Sanne, https://codepen.io/shooft/pen/rNworwK en https://www.w3schools.com/jsref/met_win_settimeout.asp
 var deCheckbox = document.querySelector("#light-mode");
 
 var deHTML = document.querySelector("html");
 
-deCheckbox.addEventListener("change", changeColorMode);
+var dingDark = new Audio('audio/ding.mp3');
 
-function changeColorMode() {
+function darkMode() {
+  // Zet de class light-mode aan en uit en speelt een geluid af
   document.documentElement.classList.toggle("light-mode");
+  dingDark.play();
 }
+
+// Als je erop klikt, voert hij de functie darkMode uit
+deCheckbox.addEventListener("change", darkMode);
